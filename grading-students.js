2f77730fffc12f83bf2f6a29f0a3fd988f1f0a8f
gsiradze <gsiradze@tula.co>
2020-07-24 20:49:23 +0400

    grading-students

---
+++ grading-students.js
@@ -0,0 +1,24 @@
+//https://www.hackerrank.com/challenges/grading/problem
+
+function gradingStudents(grades) {
+    const res = [];
+
+    grades.forEach(x => {
+       if(Math.abs(x%5 -5) < 3) {
+           console.log(x,(Math.abs(x%5) -5) );
+            const answer = Math.ceil(x/5)*5;
+            res.push(answer < 40 ? x : answer)
+        } else {
+            res.push(x);
+        }
+    });
+    return res;
+}
+
+console.log(gradingStudents([
+    4,
+    73,
+    67,
+    38,
+    33
+]));
